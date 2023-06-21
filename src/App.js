a35bfdfb97aa43762060dd66642111ee212e1958
yenglikSalamatova <yengliksalamatova@gmail.com>
2023-06-21 14:45:32 +0600

    Divided the component Steps

--- src/App.js
+++ src/App.js
@@ -7,6 +7,14 @@ const messages = [
 ];
 
 export default function App() {
+  return (
+    <div>
+      <Steps />
+    </div>
+  );
+}
+
+function Steps() {
   const [step, setStep] = useState(1);
   const [isOpen, setIsOpen] = useState(true);
 
@@ -19,7 +27,7 @@ export default function App() {
   }
 
   return (
-    <>
+    <div>
       <button className="close" onClick={() => setIsOpen((is) => !is)}>
         &times;
       </button>
@@ -49,6 +57,6 @@ export default function App() {
           </div>
         </div>
       )}
-    </>
+    </div>
   );
 }
